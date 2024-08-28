0343fa535d729c5b40cb2ad0c3c4617cc2b53e6d
Archi-Chaudhari <archic@trinesis.com>
2024-08-28 11:23:12 +0530

    initial commit

--- first.py
+++ first.py
@@ -1,1 +1,54 @@
 print('hello')
+print('hii')
+print('first line /t second line')
+
+
+m1=23
+m2=23
+m3=11
+
+#calculator
+print("addition",m1+m2+m3)
+print("sustraction",m1-m2)
+print("mul",m1*m2)
+print("division",m1/m2)
+print((m1+m2+m3)/3)
+
+#split te bill program
+total=128
+n=5
+
+share=total/n
+
+
+print("bill split is",share)
+tip_percent= 0.15
+tip_amount= total+tip_percent
+
+print("bill split is",share)
+
+days=45
+weeks=days//7
+rem=days%7
+print(f"{days}")
+
+#road trip program
+total_budget=1500
+gas_cost=600
+food_cost=300
+accomodation_cost=200
+souver=25
+
+
+
+print(gas_cost+food_cost+accomodation_cost)
+print(f"you can buy {souver}")
+
+#remainder operation in python
+print(10**2)
+
+volume=4
+print("vol of cube is",volume **3)
+
+
+
